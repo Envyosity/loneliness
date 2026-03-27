732332b7fdec91e17ccb40f88be24e83f24a77a4
Envy <leonardozkl@gmail.com>
2026-03-27 20:20:40 -0300

    removing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import BootScreen from '@/components/BootScreen/BootScreen';
 import './globals.css';
 import AshParticles from '@/components/AshParticles/AshParticles';
 import BottomBonfire from '@/components/BottomBonfire/BottomBonfire';
@@ -13,7 +12,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <BootScreen />
         <CrtOverlay />
         {children}
         <AshParticles />
